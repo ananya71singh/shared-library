29cd0a99392eb51ecfa17455adb4aad7f18eaad4
ananya71singh <75005774+ananya71singh@users.noreply.github.com>
2021-07-26 13:02:24 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -21,8 +21,8 @@ def call(String repoUrl) {
         }
         stage('deploy'){
             steps{
-                nexusArtifactUploader artifacts: [[artifactId: 'my-app', classifier: '', file: 'C:\\Windows\\System32\\config\\systemprofile\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\casestudy-1\\target\\my-app-1.0-SNAPSHOT.jar', type: 'jar']], credentialsId: 'cc14fffd-8917-4df8-b5dc-53a1207b4819', groupId: 'com.mycompany.app', nexusUrl: 'localhost:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '1.0-SNAPSHOT' }
-        }
+            nexusArtifactUploader artifacts: [[artifactId: 'my-app', classifier: '', file: 'C:\\Users\\User\\.jenkins\\jobs\\Case-study-1\\target\\my-app-1.0-SNAPSHOT.jar', type: 'jar']], credentialsId: 'eb52cb8e-67dc-40a3-aba9-96ddb7d2971d', groupId: 'com.mycompany.app', nexusUrl: 'localhost:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'http://localhost:8081/repository/SAMPLE-REl/', version: '1.0-SNAPSHOT'
+            }
        }
     post {
 failure {
